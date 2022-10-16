735394488d243e33d0a9615b96afb08fb0e0e9d2
Kevin Harrington <mad.hephaestus@gmail.com>
2022-10-16 17:39:38 -0400

    added link

--- debugDH.groovy
+++ debugDH.groovy
@@ -5,7 +5,7 @@ import com.neuronrobotics.sdk.common.DeviceManager
 
 //Your code here
 
-MobileBase mb = DeviceManager.getSpecificDevice("JackSkelington"); 
+MobileBase mb = DeviceManager.getSpecificDevice("JackSkellington"); 
 
 DHParameterKinematics kn = mb.getAllDHChains().get(0);
 
