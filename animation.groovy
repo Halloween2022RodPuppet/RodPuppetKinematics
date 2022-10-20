4056408dcb14f962f52062258a26722676bff9fe
Kevin Harrington <mad.hephaestus@gmail.com>
2022-10-20 19:52:03 -0400

    fix the audio

--- animation.groovy
+++ animation.groovy
@@ -1,1 +1,47 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.BowlerStudio
+
+import javax.sound.sampled.AudioInputStream
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.Clip
+import javax.sound.sampled.FloatControl
+
+def path = ScriptingEngine
+	.fileFromGit(
+		"https://github.com/Halloween2022RodPuppet/RodPuppetKinematics.git",//git repo URL
+		null,//branch
+		"song.wav"// File from within the Git repo
+	)
+println path
+try
+{
+	AudioInputStream audioStream = AudioSystem.getAudioInputStream(path)
+	Clip audioClip = AudioSystem.getClip();
+	audioClip.open(audioStream);
+	FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);
+	//float gainValue = (((float) config.volume()) * 40f / 100f) - 35f;
+	//gainControl.setValue(gainValue);
+
+	audioClip.start();
+	ThreadUtil.wait(10);
+	try{
+		while(audioClip.isRunning()&& !Thread.interrupted()){
+			double pos =(double) audioClip.getMicrosecondPosition()/1000.0
+			double len =(double) audioClip.getMicrosecondLength()/1000.0
+			def percent = pos/len*100.0
+			System.out.println("Current "+pos +" Percent = "+percent);
+			ThreadUtil.wait(100);
+		}
+	}catch(Throwable t){
+		BowlerStudio.printStackTrace(t)
+	}
+	audioClip.stop()
+	audioClip.close()
+	((AudioInputStream)audioStream).close()
+	
+}
+catch (Exception e)
+{
+	BowlerStudio.printStackTrace(e)
+	return null;
+}
+
