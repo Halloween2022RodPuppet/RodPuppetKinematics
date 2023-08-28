d2b4c71b192935ba637e7f1d7d03358b22d4d41d
kharrington <kharrington@192.168.17.102>
2023-08-28 11:02:15 -0400

    adding code

--- vitaminsExportToTinkercad.groovy
+++ vitaminsExportToTinkercad.groovy
@@ -1,1 +1,11 @@
 // code here
+CSG servo = Vitamins.get("hobbyServo", "mg92b")
+		.roty(-90)
+		.toZMin()
+
+CSG horn = Vitamins.get("hobbyServoHorn", "standardMicro1")		
+
+
+return [servo,horn]
+
+
